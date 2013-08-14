06122aab216c3ad1ac33288abbfcb0287f7e7552
TinaBoettger <tina.boettger@gmx.net>
2013-08-14 14:05:28 +0200

    just a Test

--- AVS-Bacteria/src/avs/Main.java
+++ AVS-Bacteria/src/avs/Main.java
@@ -18,6 +18,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+    	
         JFrame frame = new JFrame();
         frame.setTitle("AVS - Bacteria");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
