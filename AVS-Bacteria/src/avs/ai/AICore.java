0addbd8b295bf5dd47067d59a63725a8e31ab551
Cheet4h <christopher.gundlach@gmx.de>
2013-08-14 14:16:15 +0200

    added comments for further progress

--- AVS-Bacteria/src/avs/ai/AICore.java
+++ AVS-Bacteria/src/avs/ai/AICore.java
@@ -9,24 +9,37 @@ import avs.game.Cell;
 public class AICore {
 
     private GameManager gm;
+
     private boolean running = true;
+
     private LinkedList<Workload> workQueue;
+
     private GameGrid grid;
-    
+
     public void initialize(GameManager gm) {
         gm = this.gm;
     }
-    
-    public void setGameGrid(GameGrid grid){
+
+    public void setGameGrid(GameGrid grid) {
         grid = this.grid;
     }
 
     public void run() {
         LinkedList<Cell> possessedCells = grid.getCellsPossessedByAI();
-        while(!possessedCells.isEmpty()){
-            workQueue.add(new Workload(grid,possessedCells.getFirst().getX(),possessedCells.getFirst().getY()));
-            possessedCells.removeFirst();
-        }
+        while(running){
+            while (!possessedCells.isEmpty()) {
+                workQueue.add(new Workload(grid, possessedCells.getFirst().getX(), possessedCells.getFirst().getY()));
+                possessedCells.removeFirst();
+            }
+            //Distribute work
+            //collect and merge result trees
+            //get best result
+            //execute turn
+            //discard obsolete nodes
+            //get grid from leaves
+            //get cells from grids
             
+        }
+
     }
 }
