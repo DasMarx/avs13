706f259cec755fb5c3506105b712dc58fbb5486f
Fr4meJ3rk <jonas.dietz89@live.de>
2013-08-14 10:33:00 +0200

    Started functional implementation

--- AVS-Bacteria/src/avs/game/GameGrid.java
+++ AVS-Bacteria/src/avs/game/GameGrid.java
@@ -4,30 +4,20 @@
 package avs.game;
 
 import java.util.LinkedList;
+import java.util.Random;
 
 /**
  * @author HZU
- *
+ * 
  */
 public class GameGrid {
 
 	private Cell[][] gameGrid;
-	
-	
+
 	private int cellsPossessedByPlayer;
 	private int cellsPossessedByAI;
-	
-	public boolean chooseCell(int x, int y, int owner) {
-		//TODO:
-		checkTurn(x, y, owner);
-		return false;
-		//TODO: to complete
-	}
-	
-	private boolean checkTurn(int x, int y, int owner) {
-		return false;
-		//TODO: to complete
-	}
+
+	Random r;
 
 	public int getCellsPossessedByPlayer() {
 		return cellsPossessedByPlayer;
@@ -37,19 +27,25 @@ public class GameGrid {
 		return cellsPossessedByAI;
 	}
 
-	
-	public LinkedList<CellChanges> processChanges(int x, int y) {
-		//TODO:
-		return null;
-	}
-	
-	
 	public void initialize() {
-		//TODO: Initialize Gamegrid, Values, Directions, Cells, etc.
+		r = new Random();
+		gameGrid = new Cell[29][29];
+		for (int i = 0; i < 30; i++) {
+			for (int j = 0; j < 30; j++) {
+				if ((i == 0) && (j == 0))
+					gameGrid[i][j] = new Cell(i, j, -1, 0);
+				if ((i == 29) && (j == 29))
+					gameGrid[i][j] = new Cell(i, j, 1, 2);
+				gameGrid[i][j] = new Cell(i, j, 0, r.nextInt(4));
+			}
+		}
+
 	}
-	
+
 	public Cell getCell(int x, int y) {
+		if (x < 0 || y < 0 || x > 29 || y > 29)
+			return null;
 		return gameGrid[x][y];
 	}
-	
+
 }
--- AVS-Bacteria/src/avs/game/GameManager.java
+++ AVS-Bacteria/src/avs/game/GameManager.java
@@ -11,31 +11,91 @@ import avs.ui.UserInterface;
  * @author HZU
  *
  */
+
 public class GameManager {
 	private final UserInterface userInterface;
 	private GameGrid gameGrid;
-	
+	private int turn = 0;
+	private LinkedList<LinkedList<CellChanges>> allChanges = new LinkedList<LinkedList<CellChanges>>();
+
+
 	public GameManager(UserInterface userInterface) {
 		this.userInterface = userInterface;
 	}
 
 	public void run() {
-		//TODO Auto-generated method stub
 
 	}
 
-	
-	public LinkedList<CellChanges> chooseCell(int x, int y, int owner) {
-		if (gameGrid.chooseCell(x, y, owner)) {
-			LinkedList<CellChanges> changes = gameGrid.processChanges(x, y);
-//			AI starten
-			
-			return changes;
-			
+	private LinkedList<Cell> checkNeighbour(Cell c){
+		LinkedList<Cell> neighbours = new LinkedList<Cell>();
+		Cell nextNeighbour = gameGrid.getCell(c.getX(), c.getY()-1);
+		if(nextNeighbour != null){
+			if(nextNeighbour.getDirection() == 2) neighbours.add(nextNeighbour);
 		}
+		nextNeighbour = null;
+		nextNeighbour = gameGrid.getCell(c.getX()+1, c.getY());
+		if(nextNeighbour != null){
+			if(nextNeighbour.getDirection() == 3) neighbours.add(nextNeighbour);
+		}
+		nextNeighbour = null;
+		nextNeighbour = gameGrid.getCell(c.getX(), c.getY()+1);
+		if(nextNeighbour != null){
+			if(nextNeighbour.getDirection() == 0) neighbours.add(nextNeighbour);
+		}
+		nextNeighbour = null;
+		nextNeighbour = gameGrid.getCell(c.getX()-1, c.getY());
+		if(nextNeighbour != null){
+			if(nextNeighbour.getDirection() == 1) neighbours.add(nextNeighbour);
+		}
+
+		return neighbours;
+	}
+
+	public LinkedList<CellChanges> processChanges(int x, int y) {
+		LinkedList<CellChanges> changes = new LinkedList<CellChanges>
 		return null;
 	}
-	
+
+	private boolean checkTurn(int x, int y, int owner) {
+		Cell c1 = new Cell(x, y, owner, 0);
+		Cell c2 = gameGrid.getCell(x, y);
+		return c1.getOwner() == c2.getOwner();
+	}
+
+	public LinkedList<CellChanges> chooseCell(int x, int y, int owner) {
+		if ((chooseCell(x, y, owner)) != null) {
+			LinkedList<CellChanges> changes = processChanges(x, y);
+		//			AI starten
+
+		return changes;
+
+		}
+	}
+
+
+	public boolean isPlayersTurn{
+		boolean turn;
+		if()
+	}
+
+	/**
+	 * @return the Game Grid
+	 */
+	public GameGrid getGrid(){
+		synchronized(gameGrid){
+			return gameGrid;			
+		}
+	}
+
+	/**
+	 * @return last changes
+	 */
+	public LinkedList<CellChanges> getChanges(){
+		synchronized(allChanges){
+			return allChanges.getLast();
+		}
+	}
 
 	public void initialize() {
 		//TODO Auto-generated method stub
@@ -44,7 +104,7 @@ public class GameManager {
 		gameGrid = new GameGrid();
 	}
 
-		
-	
-	
+
+
+
 }
