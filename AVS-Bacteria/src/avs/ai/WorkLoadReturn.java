1ed9d49c5d99b742c9e661ddf8c0fa45b10eff5f
Cheet4h <christopher.gundlach@gmx.de>
2013-09-18 11:07:38 +0200

    Updated AI
    Doesn't work yet ...

--- AVS-Bacteria/src/avs/ai/WorkLoadReturn.java
+++ AVS-Bacteria/src/avs/ai/WorkLoadReturn.java
@@ -4,10 +4,10 @@ import java.io.Serializable;
 
 
 public class WorkLoadReturn implements Serializable{
-
     /**
      * 
      */
+    Tree<Result> resultTree;
     private static final long serialVersionUID = -2581477518340282629L;
 
 }
